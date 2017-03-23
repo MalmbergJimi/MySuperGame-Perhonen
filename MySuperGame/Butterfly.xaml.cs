91c04ddd9a50b4b07ac3e7103dc45fc8f3951644
K8718 Malmberg Jimi <K8718@student.jamk.fi>
2017-03-23 14:52:04 +0200

    Perhospeli

---
+++ MySuperGame/Butterfly.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MySuperGame
+{
+    public sealed partial class Butterfly : UserControl
+    {
+        // Animate Butterfly timer, ajastin milloin "kuvaa" vaihdetaan
+        private DispatcherTimer timer;
+
+        // Offset to show, eli mikä kohta perhosen kuvasta näytetään suorakulmion sisällä
+        private int currentFrame = 0;
+        private int direction = 1; // 1 or -1 Vaihtuuko frame "isommaksi" vai "pienemmäksi"
+        private int frameheight = 132;
+
+        // Speed, nopeus
+        private readonly double MaxSpeed = 10;      // Maxnopeus perhoselle
+        private readonly double Accelerate = 0.5;   // Kiihtyvyys
+        private double speed;
+
+        // Angel, perhosen kulma
+        private readonly double AngleStep = 5;
+        private double Angle = 0;
+
+        // Perhosen sijainti Canvaksella, collision detectionia varten julkisia.
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+
+
+        public Butterfly()
+        {
+            this.InitializeComponent();
+
+            // Animate 
+            timer = new DispatcherTimer();
+            // 125ms,   Aikaväli päivittämiselle (fps)
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 125);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            // Current frame 0,1,2,3,4   nykyinen frame perhosesta
+            if (direction == 1) currentFrame++;
+            else currentFrame--;
+            if (currentFrame == 0 || currentFrame == 4)
+            {
+                direction = -1 * direction; // 1 tai -1
+            }
+            // Set offset
+            SpriteSheetOffset.Y = currentFrame * -frameheight;
+        }
+
+        // Move -metodi
+        public void Move()
+        {
+            // More speed
+            speed += Accelerate;
+            if (speed > MaxSpeed) speed = MaxSpeed;
+
+            // Set location values (angle, speed)
+            LocationX -= (Math.Cos(Math.PI / 180 * (Angle + 90))) * speed;
+            LocationY -= (Math.Sin(Math.PI / 180 * (Angle + 90))) * speed;
+        }
+
+        public void Rotate(int direction)
+        {
+            Angle += direction * AngleStep; // -1 * 5 tai 1 * 5
+            ButterflyRotateAngle.Angle = Angle;
+        }
+
+        // Update location, päivitetään sijainti Canvaksella
+        public void SetLocation()
+        {
+            SetValue(Canvas.LeftProperty, LocationX);
+            SetValue(Canvas.TopProperty, LocationY);
+        }
+    }
+}
--- MySuperGame/MainPage.xaml.cs
+++ MySuperGame/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,161 @@ namespace MySuperGame
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // Butterfly Perhonen
+        private Butterfly butterfly;
+
+        // Flower, Flowers, Kukkia
+        private List<Flower> flowers;
+
+        // Which keys are pressed,  mitkä näppäimet ovat alaspainettuina
+        private bool UpPressed;     // Onko Ylöspäin nuolta painetty
+        private bool LeftPressed;
+        private bool RightPressed;
+
+        // Game Loop Timer
+        private DispatcherTimer timer;
+
+        // Audio, ääni
+        private MediaElement mediaElement;
+
+
+
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            // add butterfly
+            butterfly = new Butterfly
+            {
+                LocationX = MyCanvas.Width / 2,
+                LocationY= MyCanvas.Height / 2
+            };
+            // Add butterfly to Canvas, lisätään perhonen Canvakselle
+            MyCanvas.Children.Add(butterfly);
+
+            // Init list of Flowers, alustetaan kukkalista
+            flowers = new List<Flower>();
+
+            // Key Listeners,   näppäimien kuuntelua, onko jokin painettuna vai ei?
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;  // Onko näppäimi alhaalla
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;      //Onko näppäimiä "ylhäällä"
+
+            // Mouse Listeners,  hiiren kuuntelu
+            Window.Current.CoreWindow.PointerPressed += CoreWindow_PointerPressed;
+
+            // Load Audio, ladataan ääni
+            LoadAudio();
+            
+            // Start game loop,     PELI LÄHTEE HETI KÄYNTIIN
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        // Load audio when colliding has happened, ladataan audio kun törmäys on tapahtunut
+        private async void LoadAudio()
+        {
+            StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            StorageFile file = await folder.GetFileAsync("ding.wav");
+            var stream = await file.OpenAsync(FileAccessMode.Read);
+
+            mediaElement = new MediaElement();      // Ladataan audio valmiiksi muistiin, mutta ei vielä soiteta
+            mediaElement.AutoPlay = false;
+            mediaElement.SetSource(stream, file.ContentType);
+        }
+
+        private void CoreWindow_PointerPressed(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            Flower flower = new Flower();
+            flower.LocationX = args.CurrentPoint.Position.X - flower.Width / 2;     // Kukka tulee juuri hiiren kursorin kohdalle
+            flower.LocationY = args.CurrentPoint.Position.Y - flower.Width / 2;
+            // Add to Canvas, lisätään kukka näytölle
+            MyCanvas.Children.Add(flower);
+            flower.SetLocation();
+            // Add to flowers list, lisätään kukkalistaan
+            flowers.Add(flower);
+        }
+
+        //game loop
+        private void Timer_Tick(object sender, object e)
+        {
+            // Move butterfly if up pressed
+            if (UpPressed) butterfly.Move();
+
+            // Rotate butterfly if left/right pressed
+            if (LeftPressed) butterfly.Rotate(-1);   // -1 == left
+            if (RightPressed) butterfly.Rotate(1);   //  1 == right
+
+            // Update butterfly , Perhosen paikka Canvaksella päivitetään
+            butterfly.SetLocation();
+
+            // Collision...
+            CheckCollision();
+        }
+        private void CheckCollision()
+        {
+            // Loop flower list, Käydään läpi kukkalista
+            foreach(Flower flower in flowers)
+            {
+                // Get Rects, katsotaan osuuko mikään kukkalistan kukista perhoseen
+                Rect BRect = new Rect(                                               // Perhosen sijainti ja koko
+                    butterfly.LocationX, butterfly.LocationY, butterfly.ActualWidth, butterfly.ActualHeight
+                    );
+                Rect FRect = new Rect(                                               // Kukan sijainti ja koko
+                    flower.LocationX, flower.LocationY, flower.ActualWidth, flower.ActualHeight
+                    );
+                // Does objects intersects, törmääkö objektit
+                BRect.Intersect(FRect);
+                if (!BRect.IsEmpty) // Jos palautettu arvo EI OLE TYHJÄ
+                {
+                    // Collision! Area isn't empty, törmäys - alue ei ole tyhjä
+                    // Remove flower from Canvas, poistetaan Kukka Canvakselta
+                    MyCanvas.Children.Remove(flower);
+                    // Remove from list, poistetaan myös listasta kukka
+                    flowers.Remove(flower);
+                    // Play audio
+                    mediaElement.Play();
+                    
+                    break;
+                }
+
+            }
+        }
+
+        private void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)    // Tutkii tableteilta, puhelimilta, koneilta
+            {
+                case VirtualKey.Up:
+                    UpPressed = false;
+                    break;
+                case VirtualKey.Left:
+                    LeftPressed = false;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = false;
+                    break;
+
+            }
+        }
+
+        private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)    // Tutkii tableteilta, puhelimilta, koneilta
+            {
+                case VirtualKey.Up:
+                    UpPressed = true;
+                    break;
+                case VirtualKey.Left:
+                    LeftPressed = true;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = true;
+                    break;
+
+            }
         }
     }
 }
